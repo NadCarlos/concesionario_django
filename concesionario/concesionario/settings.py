075520b15e12c99c80dbbfee18e8c4da9c4788db
NadCarlos <c.nadalig@itecriocuarto.org.ar>
2024-10-17 13:37:49 -0300

    added new config for api

--- concesionario/concesionario/settings.py
+++ concesionario/concesionario/settings.py
@@ -26,10 +26,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     #Custom Apps
     'autos',
     'home',
     'usuarios',
+    'concesionario_api',
 ]
 
 MIDDLEWARE = [
